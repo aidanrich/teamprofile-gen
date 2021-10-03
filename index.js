b782380923934e6db9bb3db40737c951d6e1f721
aorich <rich.aidan@gmail.com>
2021-10-03 16:34:17 -0400

    mving files around

--- index.js
+++ index.js
@@ -1,85 +1,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+// loop code
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 // const employee = new Employee();
 
-const employeeQuestions = [
-    {
-        type: 'checkbox',
-        message: 'Please pick a role at the company',
-        name: 'role',
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        type: 'input',
-        message: 'What is the employee name?',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'What is the employee id?',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'What is the employee email?',
-        name: 'email',
-    },
-
-    {
-        type: "input",
-        name: "github",
-        message: "Enter your github username:",
-        when: (answers) => {
-            if (answers.role[1]) {
-                return true;
+const employeeQuestions = {
+    type: "loop",
+    name: "items",
+    message: "Add an employee?",
+    questions: [
+        {
+            type: 'checkbox',
+            message: 'Please pick a role at the company',
+            name: 'role',
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            type: 'input',
+            message: 'What is the employee name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is the employee id?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the employee email?',
+            name: 'email',
+        },
+    
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github username:",
+            when: (answers) => {
+                if (answers.role[1]) {
+                    return true;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        message: 'What is the managers office number?',
-        name: 'office',
-        when: (answers) => {
-            if (answers.role[0]) {
-                return true;
+        },
+        {
+            type: 'input',
+            message: 'What is the managers office number?',
+            name: 'office',
+            when: (answers) => {
+                if (answers.role[0]) {
+                    return true;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        message: 'What is the interns school?',
-        name: 'school',
-        when: (answers) => {
-            if (answers.role[2]) {
-                return true;
+        },
+        {
+            type: 'input',
+            message: 'What is the interns school?',
+            name: 'school',
+            when: (answers) => {
+                if (answers.role[2]) {
+                    return true;
+                }
             }
-        }
-    },
-]
-
-
-// loop code
-// inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
-
-// inquirer.prompt({
-//     type: "loop",
-//     name: "items",
-//     message: "Add an employee?",
-//     questions: [
-//         {
-//             type: "input",
-//             name: "key",
-//             message: "Enter item key",
-//         },
-//         {
-//             type: "input",
-//             name: "value",
-//             message: "Enter item value",
-//         },
-//     ],
-// });
+        },
+    ],
+};
 
 
 
@@ -89,7 +73,7 @@ inquirer
     .prompt(employeeQuestions)
     .then((responses) => {
 
-        fs.writeFile('./dist/index.html', `<!DOCTYPE html>
+        fs.appendFile('./dist/index.html', `<!DOCTYPE html>
                <html lang="en">
                <head>
                    <meta charset="UTF-8">
@@ -108,9 +92,9 @@ inquirer
                          <h5 class="card-title">${responses.role}</h5>
                          <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
                          <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-                         <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}${responses.githuv}${responses.school}</h6>
+                         <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}${responses.school}</h6>
                          <a href="#" class="card-link">${responses.email}</a>
-                         <a href="#" class="card-link">Another link</a>
+                         <a href="#" class="card-link">${responses.github}</a>
                        </div>
                      </div>
 
@@ -120,15 +104,3 @@ inquirer
 
 
     })
-
-    //     } else if(responses.role[1]) {
-    //         inquirer.prompt(engineerQuestions)
-    //         .then(responsesE)
-
-    //     } else (responses.role[2]) 
-    //         inquirer.prompt(internQuestions)
-    //         .then(responsesI)
-
-
-    // }});
-
