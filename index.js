5b0c229be908726e30b9125b9f37414855b0b9f9
aorich <rich.aidan@gmail.com>
2021-10-03 11:00:41 -0400

    inquirer when statements working

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ const employeeQuestions = [
         name: "github",
         message: "Enter your github username:",
         when: (answers) => {
-            if (answers.employeeTitle === "Engineer") {
+            if (answers.role[1]) {
                 return true;
             }
         }
     },
-   {
+    {
         type: 'input',
         message: 'What is the managers office number?',
         name: 'office',
         when: (answers) => {
-            if (answers.employeeTitle === "Manager") {
+            if (answers.role[0]) {
                 return true;
             }
         }
@@ -52,11 +52,11 @@ const employeeQuestions = [
         message: 'What is the interns school?',
         name: 'school',
         when: (answers) => {
-            if (answers.employeeTitle === "Intern") {
+            if (answers.role[2]) {
                 return true;
             }
         }
-    }, 
+    },
 ]
 
 // const managerQuestions = {
@@ -77,74 +77,70 @@ const employeeQuestions = [
 // };
 
 // loop code
-inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
+// inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
-inquirer.prompt({
-    type: "loop",
-    name: "items",
-    message: "Add an employee?",
-    questions: [
-        {
-            type: "input",
-            name: "key",
-            message: "Enter item key",
-        },
-        {
-            type: "input",
-            name: "value",
-            message: "Enter item value",
-        },
-    ],
-});
-
-
+// inquirer.prompt({
+//     type: "loop",
+//     name: "items",
+//     message: "Add an employee?",
+//     questions: [
+//         {
+//             type: "input",
+//             name: "key",
+//             message: "Enter item key",
+//         },
+//         {
+//             type: "input",
+//             name: "value",
+//             message: "Enter item value",
+//         },
+//     ],
+// });
 
 
 
-// inquirer
-//     .prompt(employeeQuestions)
-//     .then((responses) => {
-//         if (responses.role[0]) {
-//              (err) =>
-//             err ? console.error(err) : console.log('Commit logged!')
-//            inquirer.prompt(managerQuestions)
-//            .then((responsesM) => {
-//                fs.appendFile('newHTML.html', `<!DOCTYPE html>
-//                <html lang="en">
-//                <head>
-//                    <meta charset="UTF-8">
-//                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-//                    <title>Team Cards</title>
-//                </head>
-//                <body>
-//                    <div class="card" style="width: 18rem;">
-//                        <div class="card-body">
-//                          <h5 class="card-title">${responses.role}</h5>
-//                          <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
-//                          <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-//                          <h6 class="card-subtitle mb-2 text-muted">Office number: ${responsesM.office}</h6>
-//                          <a href="#" class="card-link">${responses.email}</a>
-//                          <a href="#" class="card-link">Another link</a>
-//                        </div>
-//                      </div>
-
-//                </body>
-//                </html>` ), (err) =>
-//                err ? console.error(err) : console.log('Commit logged!')
 
 
-//            })
+inquirer
+    .prompt(employeeQuestions)
+    .then((responses) => {
 
-//         } else if(responses.role[1]) {
-//             inquirer.prompt(engineerQuestions)
-//             .then(responsesE)
+        fs.writeFile('newHTML.html', `<!DOCTYPE html>
+               <html lang="en">
+               <head>
+                   <meta charset="UTF-8">
+                   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+                   <title>Team Cards</title>
+               </head>
+               <body>
+                   <div class="card" style="width: 18rem;">
+                       <div class="card-body">
+                         <h5 class="card-title">${responses.role}</h5>
+                         <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
+                         <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
+                         <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}</h6>
+                         <a href="#" class="card-link">${responses.email}</a>
+                         <a href="#" class="card-link">Another link</a>
+                       </div>
+                     </div>
 
-//         } else (responses.role[2]) 
-//             inquirer.prompt(internQuestions)
-//             .then(responsesI)
+               </body>
+               </html>` , (err) =>
+                err ? console.error(err) : console.log('Commit logged!'))
 
 
-//     }});
+    })
+
+    //     } else if(responses.role[1]) {
+    //         inquirer.prompt(engineerQuestions)
+    //         .then(responsesE)
+
+    //     } else (responses.role[2]) 
+    //         inquirer.prompt(internQuestions)
+    //         .then(responsesI)
+
+
+    // }});
 
