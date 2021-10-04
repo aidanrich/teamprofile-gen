c74e878231a0be91edb047123b86b021047058d9
aorich <rich.aidan@gmail.com>
2021-10-04 16:14:16 -0400

    breaking up html

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const fs = require('fs');
 inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 // const employee = new Employee();
+// const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const employeeQuestions = {
-    type: "loop",
-    name: "items",
-    message: "Add an employee?",
-    questions: [
+
+const employeeQuestions = [
+// {
+//     type: "loop",
+//     name: "items",
+//     message: "Add an employee?",
+//     questions: [
         {
             type: 'checkbox',
             message: 'Please pick a role at the company',
@@ -61,46 +67,46 @@ const employeeQuestions = {
                     return true;
                 }
             }
-        },
-    ],
-};
+        },]
+//     ],
+// };
 
 
 
 
 
-inquirer
-    .prompt(employeeQuestions)
-    .then((responses) => {
+// inquirer
+//     .prompt(employeeQuestions)
+//     .then((responses) => {
 
-        fs.appendFile('./dist/index.html', `<!DOCTYPE html>
-               <html lang="en">
-               <head>
-                   <meta charset="UTF-8">
-                   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-                   <link  rel="stylesheet" href="./style.css">
-                   <title>Team Cards</title>
-               </head>
-               <body>
-               <header class='masthead'>
-                <h1 class='masthead-heading'>The Team!</h1>
-            </header>
-                   <div class="card" style="width: 18rem;">
-                       <div class="card-body">
-                         <h5 class="card-title">${responses.role}</h5>
-                         <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
-                         <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-                         <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}${responses.school}</h6>
-                         <a href="#" class="card-link">${responses.email}</a>
-                         <a href="#" class="card-link">${responses.github}</a>
-                       </div>
-                     </div>
+//         fs.appendFile('./dist/index.html', `<!DOCTYPE html>
+//                <html lang="en">
+//                <head>
+//                    <meta charset="UTF-8">
+//                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+//                    <link  rel="stylesheet" href="./style.css">
+//                    <title>Team Cards</title>
+//                </head>
+//                <body>
+//                <header class='masthead'>
+//                 <h1 class='masthead-heading'>The Team!</h1>
+//             </header>
+                //    <div class="card" style="width: 18rem;">
+                //        <div class="card-body">
+                //          <h5 class="card-title">${responses.role}</h5>
+                //          <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
+                //          <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
+                //          <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}${responses.school}</h6>
+                //          <a href="#" class="card-link">${responses.email}</a>
+                //          <a href="#" class="card-link">${responses.github}</a>
+                //        </div>
+                //      </div>
 
-               </body>
-               </html>` , (err) =>
-                err ? console.error(err) : console.log('Commit logged!'))
+//                </body>
+//                </html>` , (err) =>
+//                 err ? console.error(err) : console.log('Commit logged!'))
 
 
-    })
+//     })
