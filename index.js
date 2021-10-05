8e5bfda555882868122c41dd36114c98c2da016c
aorich <rich.aidan@gmail.com>
2021-10-04 21:42:01 -0400

    finally got the html to generate

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function makeManager() {
                             start()
                             break;
                         case "No":
-                            // writefile
+                            renderHTML(responses)
                             break;
                         default:
                             break;
@@ -133,7 +133,7 @@ function makeEngineer() {
                             start()
                             break;
                         case "No":
-                            // writefile
+                            renderHTML(responses)
                             break;
                         default:
                             break;
@@ -176,7 +176,7 @@ function makeIntern() {
                             start()
                             break;
                         case "No":
-                            // writefile
+                            renderHTML(responses)
                             break;
                         default:
                             break;
@@ -185,8 +185,98 @@ function makeIntern() {
         })
 }
 
+
+let tempHTML = [];
+
+
+const htmlHeader = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link  rel="stylesheet" href="./style.css">
+    <title>Team Cards</title>
+</head>
+<body>
+<header class='masthead'>
+ <h1 class='masthead-heading'>The Team!</h1>
+</header>`;
+
+// complete card, might need to be broken up further
+
+const managerCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${responses.getRole()}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.getOffice()}</h6>
+  <a href="#" class="card-link">${responses.getEmail()}</a>
+  
+</div>
+</div>`}
+
+const engineerCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${responses.getRole()}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted"></h6>
+  <a href="#" class="card-link">${responses.getEmail()}</a>
+  <a href="#" class="card-link">${responses.getGithub()}</a>
+</div>
+</div>`}
+
+const internCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">${responses.getRole()}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+  <h6 class="card-subtitle mb-2 text-muted">School: ${responses.getSchool()}</h6>
+  <a href="#" class="card-link">${responses.getEmail()}</a>
+  
+</div>
+</div>`}
+
+
+const htmlFooter = `</body>`
+
+function renderHTML() {
+  tempHTML.push(htmlHeader);
+
+  tempArr.forEach(responses => {
+    
+  
+  switch (responses.getRole()) {
+    case "Manager":
+      tempHTML.push(managerCard(responses));
+      break;
+    case "Engineer":
+      tempHTML.push(engineerCard(responses));
+      break;
+    case "Intern":
+      tempHTML.push(internCard(responses));
+      break;
+    default:
+      break;
+  }});
+  tempHTML.push(htmlFooter);
+
+  tempHTML = tempHTML.join('')
+  
+
+  fs.writeFile('index.html', tempHTML.toString(), (err) =>
+  err ? console.error(err) : console.log('Commit logged!'))
+}
+
+
 start()
 
+// function makeHTML(responses) {
+//     fs.writeFile('./dist/index.html', renderHTML(responses), (err) =>
+//                     err ? console.error(err) : console.log('Commit logged!'))
+// }
 
 
 
--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,72 +1,87 @@
 // need to piece together the html page
 const fs = require('fs');
-let renderHTML = [];
+// let tempHTML = [];
 
-const htmlHeader = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-    <link  rel="stylesheet" href="./style.css">
-    <title>Team Cards</title>
-</head>
-<body>
-<header class='masthead'>
- <h1 class='masthead-heading'>The Team!</h1>
-</header>`;
 
-// complete card, might need to be broken up further
+// const htmlHeader = `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+//     <link  rel="stylesheet" href="./style.css">
+//     <title>Team Cards</title>
+// </head>
+// <body>
+// <header class='masthead'>
+//  <h1 class='masthead-heading'>The Team!</h1>
+// </header>`;
 
-const managerCard = `     <div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">${responses.role}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}</h6>
-  <a href="#" class="card-link">${responses.email}</a>
+// // complete card, might need to be broken up further
+
+// const managerCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+// <div class="card-body">
+//   <h5 class="card-title">${responses.getRole()}</h5>
+//   <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.getOffice()}</h6>
+//   <a href="#" class="card-link">${responses.getEmail()}</a>
   
-</div>
-</div>`
+// </div>
+// </div>`}
 
-const engineerCard = `     <div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">${responses.role}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.office}${responses.school}</h6>
-  <a href="#" class="card-link">${responses.email}</a>
-  <a href="#" class="card-link">${responses.github}</a>
-</div>
-</div>`
+// const engineerCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+// <div class="card-body">
+//   <h5 class="card-title">${responses.getRole()}</h5>
+//   <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">Office number: ${responses.getOffice()}</h6>
+//   <a href="#" class="card-link">${responses.getEmail()}</a>
+//   <a href="#" class="card-link">${responses.getGithub()}</a>
+// </div>
+// </div>`}
 
-const internCard = `     <div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">${responses.role}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">${responses.name}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.id}</h6>
-  <h6 class="card-subtitle mb-2 text-muted">School: ${responses.school}</h6>
-  <a href="#" class="card-link">${responses.email}</a>
+// const internCard = (responses) => { return `     <div class="card" style="width: 18rem;">
+// <div class="card-body">
+//   <h5 class="card-title">${responses.getRole()}</h5>
+//   <h6 class="card-subtitle mb-2 text-muted">${responses.getName()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">Id Number: ${responses.getId()}</h6>
+//   <h6 class="card-subtitle mb-2 text-muted">School: ${responses.getSchool()}</h6>
+//   <a href="#" class="card-link">${responses.getEmail()}</a>
   
-</div>
-</div>`
+// </div>
+// </div>`}
 
 
-const htmlFooter = `</body>`
+// const htmlFooter = `</body>`
 
-renderHTML = (tempArr) => {
-  renderHTML.push(htmlHeader)
+// renderHTML = () => {
+//   tempHTML.push(htmlHeader);
 
-  switch (key) {
-    case value:
-      
-      break;
+//   tempArr.forEach(responses => {
+    
   
-    default:
-      break;
-  }
+//   switch (responses.getRole()) {
+//     case "Manager":
+//       tempHTML.push(managerCard(responses));
+//       break;
+//     case "Engineer":
+//       tempHTML.push(engineerCard(responses));
+//       break;
+//     case "Intern":
+//       tempHTML.push(internCard(responses));
+//       break;
+//     default:
+//       break;
+//   }});
+//   tempHTML.push(htmlFooter);
 
-}
+//   tempHTML = tempHTML.join('')
+  
+
+//   fs.writeFile('index.html', tempHTML.toString(), (err) =>
+//   err ? console.error(err) : console.log('Commit logged!'))
+// }
 
 module.exports = renderHTML
